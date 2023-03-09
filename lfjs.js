e9cc0557e11fa0c89e6e5812205499338a10f3b5
natalia vieira <verkappte@hotmail.com>
2023-03-09 16:19:28 -0300

    criação da aba de nome personalize´

--- lfjs.js
+++ lfjs.js
@@ -1,3 +1,24 @@
-let foto1 = window.document.getElementsByName("arcoiris")
 
-if ()
+
+
+function passarMouse(){
+    let foto1 = window.document.getElementsByName("arcoiris")
+    window.alert("Que tal uma arte na sua orelha?")
+}
+
+function outroGender(){
+    let outroGender = window.document.getElementById("outroGender")
+    let escolhaGenero = window.confirm("Confirma não ser atendido pelas opções acima?")
+    if (escolhaGenero == true) {
+        let autoDef = window.prompt("Digite sua autodefinição")
+        if (autoDef == false) {
+            autoDef = "FULANO"
+        }
+        window.alert(`Computados sua contribuição. Consultaremos a viabilidade de criar um botão de seleção para ${autoDef}s, gratidão` )
+    } else {
+        window.alert("Obrigada!")
+    }
+}
+
+
+
